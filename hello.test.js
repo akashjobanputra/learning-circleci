4b8ef3d351747a395be90ff1f4ecd2d3d12d481e
Akash Jobanputra <akash.jobanputra@hotmail.com>
2018-10-31 00:18:48 +0530

    Breaking to check status badge on readme

--- hello.test.js
+++ hello.test.js
@@ -1,4 +1,4 @@
 const assert = require('assert');
 const hello = require('./hello.js');
 
-assert.strictEqual(hello(), 'Hello World!', 'our "Hello World" function works');
+assert.strictEqual(hello(), 'Hello World! Breaking to check status badge on readme', 'our "Hello World" function works');
