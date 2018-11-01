c29f106639a8d23bc4a266cd6471948264b74fba
AkashJobanputra <akashj@winjit.com>
2018-11-02 00:03:08 +0530

    kdfhsdjkfh

--- bin/serverSide.sh
+++ bin/serverSide.sh
@@ -2,11 +2,12 @@
 
 echo "Starting Deployment"
 # mkdir -p /home/ec2-user/learning-circleci/
-cd /home/ec2-user/
+cd /home/ec2-user/project/
 git pull 
 npm i
 npm test
+npm start
 # cd learning-circleci
-pm2 stop api
-pm2 start api
+# pm2 stop api
+# pm2 start api
 echo "Deploy End"
